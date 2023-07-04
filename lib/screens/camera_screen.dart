0a123d0952ec7e2661cecc7b5bc05a1c75ee8525
Joyal Joseph <joyaljosephnote@gmail.com>
2023-07-04 15:40:31 +0530

    camera preview added

---
+++ lib/screens/camera_screen.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  const CameraScreen({
+    Key? key,
+    required this.cameras,
+  }) : super(key: key);
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  @override
+  void initState() {
+    initializeCamera(selectedCamera); //Initially selectedCamera = 0
+    super.initState();
+  }
+
+  late CameraController _controller; //To control the camera
+  late Future<void>
+      _initializeControllerFuture; // Future to wait until camera initializes
+  int selectedCamera = 0;
+  List<File> capturedImages = [];
+
+  initializeCamera(int cameraIndex) async {
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.cameras[cameraIndex],
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // if the Future is complete, display the preview.
+                return CameraPreview(_controller);
+              } else {
+                return const Center(
+                  // Otherwise, display a loading indicator.
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
